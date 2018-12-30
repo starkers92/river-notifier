031a96348be269ffef5c302ee8355c810c6d257b
starkers92 <chris.starkey92@gmail.com>
2018-12-30 20:38:06 +1100

    changing to class like structure

--- river_scraper.py
+++ river_scraper.py
@@ -8,34 +8,58 @@ import get_url
 
 
 class River:
-    def __init__(self,name,gauge,rain_gauge):
+    def __init__(self,name,BOM_height_gauge_url,BOM_rain_gauge_url,BOM_rain_gauge_names):
         self.name=name
-        self.gauge=gauge
-        self.rain_gauge=rain_gauge
-        self.last_level=0
+        self.height_gauge=BOM_height_gauge_url
+        self.rain_gauge=BOM_rain_gauge_url
+        self.rain_gauge_names = BOM_rain_gauge_names
+        
+        self.last_height=0
         self.plot=0
+        self.time_list = []
+        self.level_list = []
     
-    def get_bom_river(self):
-        raw_html = get_url.simple_get(self.gauge)  #download river data
+    def scrape_BOM_height_list(self):
+        raw_html = get_url.simple_get(self.height_gauge)  #download river data
         
         html = BeautifulSoup(raw_html, 'html.parser') #parse using html parser
 
         raw_list=[] #search through html, all tabulated data is added to a list
         for p in html.select('td'):
             raw_list.append(p.text)
-        river_levels = raw_list[1::2] #data is in format time,level, so break it into two lists
-        river_times = raw_list[0::2]
+        self.level_list = raw_list[1::2] #data is in format time,level, so break it into two lists
+        self.time_list = raw_list[0::2]
 
         #strip years from time data
-        for i in range(0,len(river_times)):
-            tmp_str = river_times[i]
-            river_times[i] = tmp_str[0:5]+' '+tmp_str[-5:]
+        for i in range(0,len(self.time_list)):
+            tmp_str = self.time_list[i]
+            self.time_list[i] = tmp_str[0:5]+' '+tmp_str[-5:]
 
-        river_levels = [float(i) for i in river_levels] #change from str to float
+        self.level_list = [float(i) for i in self.level_list] #change from str to float
 
+        self.last_level=self.level_list[-1] #grab the last river level for an output
+
+    def scrape_BOM_rain_list(self):
+        raw_html = get_url.simple_get(self.rain_gauge)
+
+        html = BeautifulSoup(raw_html, 'html.parser') #parse using html parser
+       
+        raw_list=[] #search through html, all tabulated data is added to a list
+        my_list = []
+        last_td_tag = html.find("td")
+        while True:
+            print(last_td_tag)
+            last_td_tag = last_td_tag.next_element
+            
+        
+            
+            
+        
+        
+    def plot_height_list(self):
         fig, ax = plt.subplots(1,1) #now plot the river level, getting a tick mark every three hours
-        ax.plot(river_levels)
-        length = len(river_levels)
+        ax.plot(self.level_list)
+        length = len(self.level_list)
         xtick_time=180 #every 3 hours
         xtime_interval=15 #fixme: assuming data comes every 15 minutes, #get this from the data
         no_of_ticks_per_hour = (xtime_interval/60)
@@ -45,7 +69,7 @@ class River:
         for i in range(0,no_of_ticks):
             x_list.append(int(length*i/no_of_ticks))
             if (i%4)==0:
-                x_str_list.append(river_times[int(length*i/no_of_ticks)])
+                x_str_list.append(self.level_list[int(length*i/no_of_ticks)])
             else:
                 x_str_list.append('')
 
@@ -54,9 +78,8 @@ class River:
         ax.set_xticklabels(x_str_list,rotation=45)
         plt.show()
         self.plot=[fig,ax]
-
-        self.last_level=river_levels[-1] #grab the last river level for an output
-
     
-allyn = River('Allyn','http://www.bom.gov.au/fwo/IDN60232/IDN60232.561019.tbl.shtml',0)
-allyn.get_bom_river()
+allyn = River('Allyn','http://www.bom.gov.au/fwo/IDN60232/IDN60232.561019.tbl.shtml','http://www.bom.gov.au/cgi-bin/wrap_fwo.pl?IDN60169.html',['Mount Barrington','Careys Peak'])
+allyn.scrape_BOM_height_list()
+#allyn.plot_height_list()
+allyn.scrape_BOM_rain_list()
