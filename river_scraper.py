996d7da4cbfe2f51988e570854fa120c75e37e4c
starkers92 <chris.starkey92@gmail.com>
2019-01-19 17:14:03 +1100

    -Changed to class structure. -Added some new functions.. -Added print log

--- river_scraper.py
+++ river_scraper.py
@@ -16,15 +16,36 @@ class Area:
         gauge_strings = []
         gauge_urls = []
 
-def get_table(url):
-    raw_html = get_url.simple_get(url)
+def get_html(url):
+    print('Getting URL...')
+    try:
+        raw_html = get_url.simple_get(url)
+        print('Retrieved URL!')
+    except:
+        return
+    return raw_html
+
+def get_strings_from_html_tags(raw_html,tag):
+    print('Returning all "<', tag, '>", from html data.')
     html = BeautifulSoup(raw_html, 'html.parser') #parse using html parser
     html.prettify()
+    tag = str(tag)
+    html.find_all(tag)
+    raw_string = []
+    for i in range(0,len(html.find_all(tag))):
+        raw_string.append(html.find_all(tag)[i])
+    return raw_string    
+
+def remove_html_tags(raw_string,tag_list):
+    print('Removing "', tag_list, '" from data.')
     import re
-    str_list = []
-    for tag in html.find_all(re.compile("^td")):
-        str_list.append(str(tag.string))
-    return str_list
+    clean_string = []
+    for i in range(0,len(raw_string)):
+        temp_str = str(raw_string[i])
+        for j in range(0,len(tag_list)):
+            temp_str = re.sub(str(tag_list[j]), '',temp_str)
+        clean_string.append(temp_str)
+    return clean_string    
 
 #init class
 allyn = Area()
@@ -32,79 +53,7 @@ allyn = Area()
 allyn.River_Gauges.gauge_urls='http://www.bom.gov.au/fwo/IDN60232/IDN60232.561019.tbl.shtml'
 allyn.Rain_Gauges.gauge_urls='http://www.bom.gov.au/cgi-bin/wrap_fwo.pl?IDN60169.html'
 
-rain_list = get_table(allyn.Rain_Gauges.gauge_urls)
-print(rain_list)
-
-
-##    def __init__(self):
-##        self.river=0
-##        self.height_dict=0
-##        self.rain_gauge=0
-##        self.rain_gauge_names = 0
-##        
-##        self.last_height=0
-##        self.plot=0
-##        self.time_list = []
-##        self.level_list = []
-    
-##    def scrape_BOM_height_list(self):
-##        raw_html = get_url.simple_get(self.height_gauge)  #download river data
-##        
-##        html = BeautifulSoup(raw_html, 'html.parser') #parse using html parser
-##
-##        raw_list=[] #search through html, all tabulated data is added to a list
-##        for p in html.select('td'):
-##            raw_list.append(p.text)
-##        self.level_list = raw_list[1::2] #data is in format time,level, so break it into two lists
-##        self.time_list = raw_list[0::2]
-##
-##        #strip years from time data
-##        for i in range(0,len(self.time_list)):
-##            tmp_str = self.time_list[i]
-##            self.time_list[i] = tmp_str[0:5]+' '+tmp_str[-5:]
-##
-##        self.level_list = [float(i) for i in self.level_list] #change from str to float
-##
-##        self.last_level=self.level_list[-1] #grab the last river level for an output
-
-##    def scrape_BOM_rain_list(self):
-##        raw_html = get_url.simple_get(self.rain_gauge)
-##
-##        html = BeautifulSoup(raw_html, 'html.parser') #parse using html parser
-##        #html.prettify()
-##        raw_string_list = []
-##        for string in html.stripped_strings:
-##            raw_string_list.append(string)
-  
-        
-##    def plot_height_list(self):
-##        fig, ax = plt.subplots(1,1) #now plot the river level, getting a tick mark every three hours
-##        ax.plot(self.level_list)
-##        length = len(self.level_list)
-##        xtick_time=180 #every 3 hours
-##        xtime_interval=15 #fixme: assuming data comes every 15 minutes, #get this from the data
-##        no_of_ticks_per_hour = (xtime_interval/60)
-##        no_of_ticks = int(xtick_time*no_of_ticks_per_hour)
-##        x_list = []
-##        x_str_list = []
-##        for i in range(0,no_of_ticks):
-##            x_list.append(int(length*i/no_of_ticks))
-##            if (i%4)==0:
-##                x_str_list.append(self.level_list[int(length*i/no_of_ticks)])
-##            else:
-##                x_str_list.append('')
-##
-##        ax.set_xticks(x_list)
-##        plt.grid()
-##        ax.set_xticklabels(x_str_list,rotation=45)
-##        plt.show()
-##        self.plot=[fig,ax]
-
-        
-    
-#allyn = River('Allyn','http://www.bom.gov.au/fwo/IDN60232/IDN60232.561019.tbl.shtml','http://www.bom.gov.au/cgi-bin/wrap_fwo.pl?IDN60169.html',['Mount Barrington','Careys Peak'])
-#allyn.scrape_BOM_height_list()
-#allyn.plot_height_list()
-#allyn.scrape_BOM_rain_list()
-
-
+html_data = get_html(allyn.Rain_Gauges.gauge_urls)
+raw_html_strings = get_strings_from_html_tags(html_data,'td')
+tags_to_remove = ['<td align="left">','<td align="right">','</td>','<td>','\xa0','\n','<h4>','</h4>','<h3>','</h3>']
+cleaned_html_strings = remove_html_tags(raw_html_strings,tags_to_remove)
